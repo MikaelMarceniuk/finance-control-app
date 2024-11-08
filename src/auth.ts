8b93c8a4a72562975524c7613d3f358695d8ad90
Mikael Marceniuk <mika.marceniuk@gmail.com>
2024-11-08 01:32:00 -0300

    feat(Auth): Sending providerAccountId for clientside

--- src/auth.ts
+++ src/auth.ts
@@ -3,4 +3,22 @@ import GitHub from 'next-auth/providers/github'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
 	providers: [GitHub],
+	callbacks: {
+		async jwt({ token, account }) {
+			// Persists providerAccountId on signIn
+			if (account) {
+				token.id = account.providerAccountId
+			}
+
+			return token
+		},
+		async session({ session, token }) {
+			// Sends providerAccountId from Token to client
+			if (token.id) {
+				session.user.id = token.id
+			}
+
+			return session
+		},
+	},
 })
